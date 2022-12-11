806b3cebcc33f78d8994b133fe051314bbbf3f29
mayank9200 <64470374+mayank9200@users.noreply.github.com>
2022-12-11 15:03:29 +0530

    Time: 2700 ms (15.49%), Space: 15.5 MB (31.46%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.py
@@ -0,0 +1,29 @@
+class Solution:
+    import math
+    def solve(self,num):
+        fact=[]
+        summ=0
+        for i in range(1,int(math.sqrt(num))+1):
+            if num%i==0:
+                if i!=num//i:
+                    fact.append(i)
+                    fact.append(num//i)
+                else:
+                    fact.append(i)
+                summ=summ+i+num//i
+        #print(fact)        
+        if len(fact)==4:
+            return summ
+        return 0
+            
+        
+    def sumFourDivisors(self, nums: List[int]) -> int:
+        ans=0
+        for i in nums:
+            temp=self.solve(i)
+            #print(temp)
+            if temp!=0:
+                ans+=temp
+        return ans        
+        
+        
