dde51fe8cb52c086ec9c6633edd8ce730b12e6f3
mayank9200 <64470374+mayank9200@users.noreply.github.com>
2022-12-22 02:59:15 +0530

    Time: 78 ms (72.52%), Space: 14.1 MB (55.00%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        d={}
+        for i in magazine:
+            d[i]=d.get(i,0)+1
+        for i in ransomNote:
+            if i not in d:
+                return False
+            d[i]-=1
+            if d[i]==0:
+                d.pop(i)
+        return True        
+        
