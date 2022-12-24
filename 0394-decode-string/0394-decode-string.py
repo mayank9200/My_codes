626f03297ebb6c4b6c06d57f48da8317b3345480
mayank9200 <64470374+mayank9200@users.noreply.github.com>
2022-12-25 01:20:36 +0530

    Time: 32 ms (88.94%), Space: 14 MB (20.66%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,4 +1,5 @@
 class Solution:
+    #Take 2 stacks, one is freq stack and other is char stack,as soon as we see ],pop top of char stack and frequency stack and appendleft result in char stack, (dont forget to push whole number in freq stack rather than single digit, check line 13)
     def decodeString(self, s: str) -> str:
         from collections import deque
         s1=deque()
@@ -9,13 +10,11 @@ class Solution:
             if s[i].isdigit():
                 j=i
                 val=''
-                while s[j].isdigit():
+                while s[j].isdigit(): #calculating number
                     val=val+s[j]
                     j+=1
                 i=j-1
-                s1.appendleft(int(val))
-                
-                
+                s1.appendleft(int(val))   
             elif s[i]==']':
                 temp1=''
                 while s2[0]!='[':
@@ -24,13 +23,6 @@ class Solution:
                 s2.popleft()
                 temp2=s1.popleft()
                 s2.appendleft(temp2*temp1)
-                # mul=''
-                # while len(s1)!=0:
-                #     mul=str(s1[0])+mul
-                #     s1.popleft()
-                # #temp2=int(mul)
-                # print(mul)
-                #s2.appendleft(temp1*temp2)
             else:
                 s2.appendleft(s[i])
             i+=1    
