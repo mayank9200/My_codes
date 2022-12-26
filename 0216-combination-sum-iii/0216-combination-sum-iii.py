ee7c8b849757f70cb68a2905c04affdaec13e5f0
mayank9200 <64470374+mayank9200@users.noreply.github.com>
2022-12-26 15:45:59 +0530

    Time: 39 ms (76.41%), Space: 13.9 MB (79.59%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -3,13 +3,15 @@ class Solution:
         if target<0:
             return
         if target==0 and len(ans)==k:
-            res.append(ans)
+            res.append(ans[:])
             return 
         if val>9:
             return 
         if len(ans)>k:
             return
-        self.rec(val+1,target-val,k,ans+[val],res)
+        ans.append(val)
+        self.rec(val+1,target-val,k,ans,res)
+        ans.pop()
         self.rec(val+1,target,k,ans,res)
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         val=1
