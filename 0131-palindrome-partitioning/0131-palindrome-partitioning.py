1fbf5350a767bea813a5e7aac3f25accb1072710
mayank9200 <64470374+mayank9200@users.noreply.github.com>
2022-12-26 12:23:29 +0530

    Time: 691 ms (84.55%), Space: 29.1 MB (82.16%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,30 @@
+class Solution:
+    def ispalin(self,s):
+        start=0
+        end=len(s)-1
+        while start<=end:
+            if s[start]!=s[end]:
+                return False
+            start+=1
+            end-=1
+        return True
+    def rec(self,ip,ans,res,):
+        if len(ip)==0:
+            res.append(ans)
+            return 
+        
+        for i in range(len(ip)):   
+            first_half=ip[0:i+1]
+            second_half=ip[i+1:]
+            if self.ispalin(first_half):  
+                self.rec(second_half,ans+[first_half],res)
+
+            
+            
+        
+    def partition(self, s: str) -> List[List[str]]:
+        op=''
+        res=[]
+        ans=[]
+        self.rec(s,ans,res)
+        return res
