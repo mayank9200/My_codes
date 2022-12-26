1d8e2c0c48465f7c78ffa7741346ead1164df31c
mayank9200 <64470374+mayank9200@users.noreply.github.com>
2022-12-26 16:01:40 +0530

    Time: 100 ms (65.90%), Space: 13.9 MB (94.38%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,19 +1,21 @@
 class Solution:
-    def rec(self,index,arr,target,ans,res):
+    def rec(self,pos,candidates,target,ans,res):
         if target<0:
+            return
+        if target==0:
+            res.append(ans)
+            return
+        if pos==len(candidates):
             return 
-        if index==len(arr):
-            if target==0:
-                res.append(ans)
-                return     
-            return        
-        self.rec(index,arr,target-arr[index],ans+[arr[index]],res)
-        self.rec(index+1,arr,target,ans,res)
-        
+        self.rec(pos,candidates,target-candidates[pos],ans+[candidates[pos]],res)
+        while pos+1<len(candidates) and candidates[pos]==candidates[pos+1]:
+            pos+=1
+        self.rec(pos+1,candidates,target,ans,res)
         
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans=[]
         res=[]
+        candidates.sort()
         self.rec(0,candidates,target,ans,res)
         return res
         
