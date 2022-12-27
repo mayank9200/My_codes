e8da5c252837d36349539bc7224e6bed08a46bc8
mayank9200 <64470374+mayank9200@users.noreply.github.com>
2022-12-27 08:39:55 +0530

    Time: 30 ms (97.74%), Space: 14.1 MB (98.66%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,18 +1,9 @@
 class Solution:
-    def ans(self,op):
-        temp=[]
-        for i in op:
-            temp.append(i)
-        return temp    
-    #import copy
     def rec(self,op,res,openb,closedb):
         if openb<0 or closedb<0:
             return
         if openb==0 and closedb==0:
-            #print(op)
-            #val=self.ans(op)
-            res.append(op)
-            #res.append(''.join(self.ans(op)))
+            res.append(op)    
             return 
         self.rec(op+'(',res,openb-1,closedb)
         if openb<closedb:
