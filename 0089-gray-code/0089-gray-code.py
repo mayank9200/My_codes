c8dab951683ddc4fb3a0bf655c040d691609993e
mayank9200 <64470374+mayank9200@users.noreply.github.com>
2022-12-27 12:02:37 +0530

    Time: 140 ms (74.59%), Space: 21.3 MB (70.33%) - LeetHub

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -1,16 +1,26 @@
 class Solution:
+    
     def grayCode(self, n: int) -> List[int]:
-        res=[]
+        # res=[]
+        # if n==0:
+        #     retrun [0]
+        # res.append(0)
+        # res.append(1)
+        # for i in range(1,n):
+        #     temp=pow(2,i)
+        #     l=len(res)
+        #     for j in range(l-1,-1,-1):
+        #         res.append(res[j]+temp)
+        # return res   
         if n==0:
-            retrun [0]
-        res.append(0)
-        res.append(1)
-        for i in range(1,n):
-            temp=pow(2,i)
-            l=len(res)
-            for j in range(l-1,-1,-1):
-                res.append(res[j]+temp)
-        return res    
-            
+            return [0]
+        if n==1:
+            return [0,1]
+        asf=self.grayCode(n-1)
+        l=len(asf)
+        for i in range(l-1,-1,-1):
+            temp=pow(2,n-1)
+            asf.append(asf[i]+temp)
+        return asf    
             
         
