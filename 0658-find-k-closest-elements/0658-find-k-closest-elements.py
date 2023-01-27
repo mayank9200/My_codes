6c20d3dd38f89275a12adbc239574265bb807d6b
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-01-27 14:50:11 +0530

    Time: 309 ms (78.19%), Space: 15.5 MB (42.90%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,15 +1,59 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        from heapq import heappush,heappop
-        h=[]
-        ans=[]
+        #Heap technique
+        # from heapq import heappush,heappop
+        # h=[]
+        # ans=[]
+        # n=len(arr)
+        # for i in range(n):
+        #     heappush(h,[abs(arr[i]-x),i])
+        # while len(h)>0 and k>0:
+        #     k=k-1
+        #     temp=heappop(h)
+        #     ans.append(arr[temp[1]])
+        # ans.sort()
+        # return ans
+        
+        #Binary search + two pointer
         n=len(arr)
-        for i in range(n):
-            heappush(h,[abs(arr[i]-x),i])
-        while len(h)>0 and k>0:
-            k=k-1
-            temp=heappop(h)
-            ans.append(arr[temp[1]])
+        ans=[]
+        low=0
+        high=n-1
+        mid=0
+        while low<=high:
+            mid=(low+high)//2
+            if arr[mid]==x:
+                break
+            elif x<arr[mid]:
+                high=mid-1
+            else:
+                low=mid+1 
+        
+        if mid==0:
+            low=0
+            high=1
+        else:
+            low=mid-1
+            high=mid
+        while low >=0 and high<n and len(ans)<k:
+     
+            if abs(arr[low]-x)==abs(arr[high]-x):
+                ans.append(arr[low])
+                low-=1
+            elif abs(arr[low]-x)<abs(arr[high]-x):
+                ans.append(arr[low])
+                low-=1
+            else:
+                ans.append(arr[high])
+                high+=1   
+        while low>=0 and len(ans)<k:
+           
+            ans.append(arr[low])
+            low-=1
+        while high<n and len(ans)<k:
+       
+            ans.append(arr[high])
+            high+=1    
         ans.sort()
-        return ans
-                     
+        return ans        
+            
