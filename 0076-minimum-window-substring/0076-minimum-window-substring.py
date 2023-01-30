d7ecca96df96608249a2313681a4651afd019a8b
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-01-30 17:09:27 +0530

    Time: 511 ms (23.32%), Space: 14.6 MB (78.40%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,9 +1,9 @@
 class Solution:
     def compare(self,d1,d2):
         for i in d2:
-            if d1[i]<d2[i]:
+            if d1[i]<d2[i]: #desired number of letters are not yet covered
                 return False
-        return True    
+        return True  #contains minimum required letters with appropriate frequency
         
     def minWindow(self, s: str, t: str) -> str:
         d1={}
@@ -22,8 +22,8 @@ class Solution:
                     d1[s[j]]+=1
                 j+=1   
             while i<=j and self.compare(d1,d2)==True:
-                if j-i+1<minn:
-                    minn=j-i+1
+                if j-i<minn:
+                    minn=j-i
                     val=s[i:j]
                 if s[i] in d1:
                     d1[s[i]]-=1
