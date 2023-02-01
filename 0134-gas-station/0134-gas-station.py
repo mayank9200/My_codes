10357c74443ef24f1cd71928288292fd93e10cac
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-02-01 16:23:14 +0530

    Time: 1072 ms (13.25%), Space: 19.9 MB (10.89%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,10 +1,7 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        gassum=sum(gas)
-        costsum=sum(cost)
-        if gassum<costsum:
-            return -1
         currpet=0
+        prevpet=0
         i=0
         start=0
         n=len(gas)
@@ -12,8 +9,9 @@ class Solution:
             currpet=currpet+(gas[i]-cost[i])
             if currpet<0:
                 start=i+1
+                prevpet=prevpet+currpet
                 currpet=0
-        if currpet>=0:
+        if currpet+prevpet>=0:
             return start
         return -1
         
