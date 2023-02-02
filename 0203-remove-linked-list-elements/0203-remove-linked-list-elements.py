8704f9e1529bb80be7ee2bda5c77da3103a50baa
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-02-02 15:33:30 +0530

    Time: 77 ms (48.73%), Space: 17.8 MB (78.34%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -5,15 +5,14 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        if head==None:
-            return None
-        thead=head
+        dummy=ListNode(-1) #just to be sure ki first element se execution ho
+        dummy.next=head #insert dummy in beginning
+        head=dummy #make dummy as new head
+        thead=head  
         while thead.next!=None:
             if thead.next.val==val:
                 thead.next=thead.next.next
             else:
                 thead=thead.next
-        if head.val==val:
-            return head.next
-        return head        
+        return head.next       #since head to dummy hai actual list next se shuru hai
         
