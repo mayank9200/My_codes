33e2e1ccf88e8369749f69387e541512ef15a3c0
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-02-06 20:19:53 +0530

    Time: 29 ms (99.63%), Space: 13.8 MB (98.34%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -10,13 +10,13 @@ class Solution:
         prev=dummy
         curr=head
         while curr!=None and curr.next!=None:
-            if curr.val!=curr.next.val:
+            if curr.val!=curr.next.val: #move only if next two values are unequal
                 prev=prev.next
                 curr=curr.next
             else:
-                tval=curr.val
+                tval=curr.val #else move till repeated values occur
                 while curr!=None and curr.val==tval:
                     curr=curr.next
-            prev.next=curr
+            prev.next=curr #prev ko curr se link bhi krna hai, use copy pen
         return dummy.next    
         
