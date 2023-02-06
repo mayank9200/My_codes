317f380652b2c03072288ff2be2957723135bcf4
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-02-06 19:06:39 +0530

    Time: 155 ms (88.72%), Space: 14.3 MB (99.01%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -4,14 +4,14 @@ class Solution:
         summ=0
         count=0
         for i in boxTypes:
-            if count+i[0]<truckSize:
+            if count+i[0]<truckSize: 
                 count=count+i[0]
-                summ=summ+i[1]*i[0]
-                print(summ)
-            else:
-                val=truckSize-count
-                summ=summ+i[1]*val
+                summ=summ+i[1]*i[0] #sab rakh do 
                 #print(summ)
-                count=count+i[0]
+            else:
+                val=truckSize-count #find remaining boxes
+                summ=summ+i[1]*val #keep that only
+                #print(summ)
+                count=count+i[0]  
                 break
         return summ       
