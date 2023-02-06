9f7f7f01a11c03016f7c92591255a8da41e873a9
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-02-07 01:12:51 +0530

    Time: 46 ms (92.67%), Space: 18.5 MB (87.29%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -5,12 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def solve(self,root,flag):
+    def solve(self,root,flag): #simple height calculate function
         if root==None:
             return 0
         lh=self.solve(root.left,flag)
         rh=self.solve(root.right,flag)
-        if abs(lh-rh)>1:
+        if abs(lh-rh)>1: #global variable ek baar bhi height me 1 se jyada ka diff
             flag[0]=False
         return 1+max(lh,rh)
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
