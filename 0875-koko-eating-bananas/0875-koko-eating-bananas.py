c0fb1f2dba710acfff31fdae14f0290ebf436339
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-02-06 19:02:32 +0530

    Time: 474 ms (77.94%), Space: 15.5 MB (66.96%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -2,20 +2,20 @@ class Solution:
     def ispossible(self,mid,piles,h):
         count=0
         for i in piles:
-            count=count+i//mid
-            tval=i%mid
-            if tval>0:
+            count=count+i//mid # kitne hours to eat that particular piles a 'mid' speed 
+            tval=i%mid #if something is remaining then add one
+            if tval>0:  #basically take ceil value in case of decimal
                 count+=1
-        return count<=h     
+        return count<=h     # was the time taken less than s
     
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         start=1
         end=max(piles)+1
         while start<end:
             mid=(start+end)//2
-            if self.ispossible(mid,piles,h):
-                candi=mid
-                end=mid
+            if self.ispossible(mid,piles,h): #do we have a ans
+                candi=mid #candidate
+                end=mid # search for any better ans
             else:
                 start=mid+1
         return candi        
