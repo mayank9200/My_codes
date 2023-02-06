51df9cbd5c0f430c8a682827f8727bc259bf1c50
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-02-06 19:09:24 +0530

    Time: 379 ms (70.91%), Space: 17 MB (92.73%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -4,19 +4,16 @@ class Solution:
         for i in range(len(profit)):
             arr.append([difficulty[i],profit[i]])
         arr.sort()
-        worker.sort()      
-        maxx=0
-        for i in arr:
-            maxx=max(maxx,i[1])
+        worker.sort() #important
         summ=0
         i=0
         maxx=0
         tot=0
         n=len(profit)
         for work in worker:
-            while i<n and arr[i][0]<=work:
+            while i<n and arr[i][0]<=work: #greedy
                 maxx=max(maxx,arr[i][1])
                 i+=1
-            tot=tot+maxx      
+            tot=tot+maxx   #final ans loading   
         return tot
         
