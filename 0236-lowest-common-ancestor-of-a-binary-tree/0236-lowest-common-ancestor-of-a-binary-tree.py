91bac89f5a5d9ac3c8424a86e15dd0d2a857ae96
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-02-09 04:29:49 +0530

    Time: 73 ms (70.84%), Space: 26.3 MB (28.24%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,18 +6,18 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root==None:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':# 4 cases
+        if root==None: #if both of them are neither in left or in right
             return None
         if root==p or root==q:
             return root
-        lca1=self.lowestCommonAncestor(root.left,p,q)
-        lca2=self.lowestCommonAncestor(root.right,p,q)
-        if lca1!=None and lca2!=None:
+        lca1=self.lowestCommonAncestor(root.left,p,q) #simply gives lca from left if exist
+        lca2=self.lowestCommonAncestor(root.right,p,q) #simply gives lca from right if exist
+        if lca1!=None and lca2!=None: #if both of them are either in left subtree or right subtree
             return root
-        if lca1==None:
+        if lca1==None: #if both are in left only
             return lca2
-        else:
+        else: #if both are in right only
             return lca1
             
         
