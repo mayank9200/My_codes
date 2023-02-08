a8b4d88d0975265e1fb8d7102e5823d95b9d60fc
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-02-09 05:07:29 +0530

    Time: 29 ms (92.12%), Space: 14.1 MB (90.39%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -5,23 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    #level order traversal
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
         from collections import deque
         if root==None:
             return 0
         q=deque()
-        q.append([root,False])
+        q.append([root,False]) #[root,isleft] isleft will. be true only when it is left node
         summ=0
         while len(q)>0:
             size=len(q)
             for i in range(size):
                 node,isleft=q.popleft()
-                if node.left==None and node.right==None and isleft==True:
+                if node.left==None and node.right==None and isleft==True:#just one condition if it is left node and both of its child are None then only sum
                     summ+=node.val
                 if node.left:
-                    q.append([node.left,True])
+                    q.append([node.left,True]) #when insert left mark isleft as True
                 if node.right:
-                    q.append([node.right,False])
+                    q.append([node.right,False]) #when insert right mark isleft as False
         return summ            
                     
         
