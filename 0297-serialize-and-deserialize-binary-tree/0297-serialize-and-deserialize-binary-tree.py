6f67d14196ed384e0d9a1fad81fad081a0834204
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-02-09 05:58:54 +0530

    Time: 122 ms (67.38%), Space: 20.1 MB (92.29%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -6,21 +6,21 @@
 #         self.right = None
 
 class Codec:
-
+    #simple level order traversal
     def serialize(self, root):
         from collections import deque
         q=deque()
         q.append(root)
         res=''
-        while len(q)>0:
+        while len(q)>0: 
             size=len(q)
             for i in range(size):
                 node=q.popleft()
                 if node==None:
-                    res=res+'#,'
+                    res=res+'#,' #if none so add # and a comma,comma because it acts as a separator while deserializing
                     break
                 else:
-                    res=res+str(node.val)+','
+                    res=res+str(node.val)+',' #adding value and comma,comma because it acts as a separator while deserializing
                 q.append(node.left)
                 q.append(node.right)
         return res        
@@ -38,26 +38,27 @@ class Codec:
         
 
     def deserialize(self, data):
-        data=data.split(',')
-        if data[0]=='#':
+        #traverse the string and make connections
+        data=data.split(',') #split data in a char array to know actual numbers(cases like -1,-2001)
+        if data[0]=='#': #if first is only none
             return None
         from collections import deque
         q=deque()
         k=0
-        root=TreeNode(data[k])
+        root=TreeNode(data[k]) #make actual nodes now
         k+=1
         q.append(root)
         while len(q)>0:
             size=len(q)
             for i in range(size):
                 node=q.popleft()
-                if data[k]!='#':
+                if data[k]!='#': #if some value is there then make connections
                     node.left=TreeNode(int(data[k]))
                     q.append(node.left)
                 
                     
                 k+=1
-                if data[k]!='#':
+                if data[k]!='#': #if some value is there then make connections
                     node.right=TreeNode(int(data[k]))
                     q.append(node.right)
                   
