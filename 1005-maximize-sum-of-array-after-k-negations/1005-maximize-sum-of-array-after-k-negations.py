e8d483fa890e8299be539b06db372ff16198c5a6
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-02-26 19:38:17 +0530

    Time: 54 ms (59.30%), Space: 14 MB (46.61%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
@@ -1,20 +1,20 @@
 class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
         n=len(nums)
-        nums.sort()
+        nums.sort() #sort
         for i in range(n):
-            if k==0:
+            if k==0: #break at 0
                 break
             if nums[i]<0:
-                nums[i]=-1*nums[i]
+                nums[i]=-1*nums[i] #starting se positive banate jao
                 k=k-1
         minn=float('inf')  
         summ=0
         for i in range(n):
             summ+=nums[i]
-            minn=min(minn,nums[i])
+            minn=min(minn,nums[i]) #find what is current minimum
         if k%2!=0:
-            return summ-2*minn
+            return summ-2*minn #subtract two times as it was added earlier
         else:
             return summ            
         
