c5e2761a5961bfbf5c82b719380603f8b7c605ee
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-02-26 17:04:33 +0530

    Time: 451 ms (52.43%), Space: 27.3 MB (68.13%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
+        #total me se n-k window subtract krke jo max ans ho, try to visualize its easy
         total=sum(cardPoints)
         wind_sum=0
         n=len(cardPoints)
@@ -9,11 +10,11 @@ class Solution:
         j=(n-k-1)
         ans=0
         while j<n:
-            ans=max(ans,total-wind_sum)
+            ans=max(ans,total-wind_sum) #check first case
             if j+1<n:
-                wind_sum+=cardPoints[j+1]
+                wind_sum+=cardPoints[j+1] #add j
             if i+1<n:
-                wind_sum-=cardPoints[i]
+                wind_sum-=cardPoints[i] #remove i
             j+=1
             i+=1
         return ans    
