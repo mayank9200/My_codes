4bbcec585d6afc435420a40fbffeb3a696422ba7
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-02-27 22:20:42 +0530

    Time: 51 ms (83.67%), Space: 15.4 MB (54.29%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -8,17 +8,21 @@ class Solution:
     def solve(self,root,d,summ,target,count):
         if root==None:
             return 
-        summ=summ+root.val
-        if summ-target in d:
-            count[0]+=d[summ-target]
-        d[summ]=d.get(summ,0)+1
+        summ=summ+root.val #summ till now, basically prefix sum
+        if summ-target in d: #got one ans
+            count[0]+=d[summ-target] #how many times we got subarrays earlier, sabke sath combinations banenge
+        d[summ]=d.get(summ,0)+1 #increment the prefix sum
         self.solve(root.left,d,summ,target,count)
         self.solve(root.right,d,summ,target,count)
-        d[summ]-=1
-        
+        d[summ]-=1 #backtrack, decrease the prefix sum
+        if d[summ]==0: 
+            d.pop(summ)
+            
             
             
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        #https://www.youtube.com/watch?v=yyZA4v0x16w
+        #similar to subarray sum of size k problem
         d={}
         d[0]=1 # for the cases of prefix sum as 0, you can also add another if condition instead of this
         count=[0]
