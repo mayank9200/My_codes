1223cba7a221499b750c78b6dc225a3190cfdbe2
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-01 00:28:28 +0530

    Time: 103 ms (61.13%), Space: 14.3 MB (88.80%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.py
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.py
@@ -1,9 +1,9 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        
-        people.sort(key=lambda a: [-a[0],a[1]])
+        people.sort(key=lambda a: [-a[0],a[1]]) #first based on height then on k
         queue = []
         #print(people)
+        #next step is just like insertion sort
         for p in people:
             queue.insert(p[1], p)
         return queue
