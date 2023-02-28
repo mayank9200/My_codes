852fa1a788899421fd305bcf5133daf10980ce3c
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-01 03:00:46 +0530

    Time: 507 ms (14.10%), Space: 14.4 MB (57.05%) - LeetHub

--- 1439-find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows/1439-find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows.py
+++ 1439-find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows/1439-find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows.py
@@ -5,24 +5,24 @@ class Solution:
         h=[]
         m=len(mat)
         n=len(mat[0])
-        for i in mat[0]:
+        for i in mat[0]: #simply put qst row in queue
             q.append(i)
         row=0    
-        while len(q)>0:
+        while len(q)>0: #bfs apply
             size=len(q)
             row+=1
-            if row==m:
+            if row==m: #if we beyond last row
                 break
             for i in range(size):
                 val=q.popleft()
-                for col in range(n):
+                for col in range(n): #making combinations of elements in queue and next row values
                     q.append(val+mat[row][col])
             ans=[]
-            while len(q)>0:
+            while len(q)>0: #putting queue in array so that we can sort it
                 ans.append(q.popleft())
-            ans.sort()
+            ans.sort() #sort
             if len(ans)>k:
-                ans=ans[:k]
-            for i in ans:
+                ans=ans[:k] #keep values till k only in queue
+            for i in ans: #put back in queue
                 q.append(i)
-        return q[-1]
+        return q[-1] #last element of queue is our ans
