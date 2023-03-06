b9eff3558ccb02cec6cb1c510afa308d9b61302c
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-06 12:40:32 +0530

    Time: 788 ms (76.47%), Space: 30.6 MB (30.49%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def check(self,nums,n,summ,dp):
+        if n==0:
+            return False
+        if summ<0:
+            return False
+        if summ==0:
+            return True
+        if dp[n][summ]!=-1:
+            return dp[n][summ]
+        if nums[n-1]<=summ:
+            dp[n][summ]=self.check(nums,n-1,summ-nums[n-1],dp) or self.check(nums,n-1,summ,dp)
+            return dp[n][summ]
+        else:
+            dp[n][summ]=self.check(nums,n-1,summ,dp)
+            return dp[n][summ]
+            
+    def canPartition(self, nums: List[int]) -> bool:
+        summ_of_nums=sum(nums)
+        if summ_of_nums%2!=0:
+            return False
+        dp=[[-1 for i in range(summ_of_nums//2+1)] for j in range(len(nums)+1)]
+        #print(dp)
+        return self.check(nums,len(nums),summ_of_nums//2,dp)
+        
+        
