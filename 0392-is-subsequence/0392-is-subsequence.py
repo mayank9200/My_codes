1eeb1ade8a88b9e63128468aa7bf4c6019fdfc01
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-07 19:11:27 +0530

    Time: 39 ms (31.35%), Space: 14 MB (8.34%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,17 +1,29 @@
 class Solution:
-    def solve(self,s1,s2,m,n,dp):
-        if m==0 or n==0:
-            return 0
-        if dp[m][n]!=-1:
-            return dp[m][n]
-        if s1[m-1]==s2[n-1]:
-            dp[m][n]=1+self.solve(s1,s2,m-1,n-1,dp)
-        else:
-            dp[m][n]=max(self.solve(s1,s2,m-1,n,dp),self.solve(s1,s2,m,n-1,dp))
-        return dp[m][n]    
+    # def solve(self,s1,s2,m,n,dp):
+    #     if m==0 or n==0:
+    #         return 0
+    #     if dp[m][n]!=-1:
+    #         return dp[m][n]
+    #     if s1[m-1]==s2[n-1]:
+    #         dp[m][n]=1+self.solve(s1,s2,m-1,n-1,dp)
+    #     else:
+    #         dp[m][n]=max(self.solve(s1,s2,m-1,n,dp),self.solve(s1,s2,m,n-1,dp))
+    #     return dp[m][n]    
     def isSubsequence(self, s: str, t: str) -> bool:
+        # m,n=len(s),len(t)
+        # dp=[[-1 for i in range(n+1)] for j in range(m+1)]
+        # return len(s)==self.solve(s,t,m,n,dp) #if lcs is s and t is s
+        i=0
+        j=0
         m,n=len(s),len(t)
-        dp=[[-1 for i in range(n+1)] for j in range(m+1)]
-        return len(s)==self.solve(s,t,m,n,dp) #if lcs is s and t is s
+        count=0
+        while i<m and j<n:
+            if s[i]==t[j]:
+                count+=1
+                i+=1
+                j+=1
+            else:
+                j+=1
+        return count==len(s)        
         
         
