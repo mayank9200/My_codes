00f5de09f1eb291c38c8635ccfa7ddf9bca77455
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-09 00:42:34 +0530

    Time: 61 ms (63.22%), Space: 14.1 MB (18.99%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,12 +1,12 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n=len(cost)
-        dp=[0 for i in range(n)]
+        dp=[0 for i in range(n)] #see till n step 
         if n<=2:
-            return min(cost[0],cost[1])     
+            return min(cost[0],cost[1]) #to ease the loop
         dp[0]=cost[0]
         dp[1]=cost[1]
-        for i in range(2,n):
-            dp[i]=min(dp[i-1]+cost[i],dp[i-2]+cost[i])
-        return min(dp[n-1],dp[n-2])    
+        for i in range(2,n): #traverse and and at point store how much is cost required to reach at that point
+            dp[i]=min(dp[i-1]+cost[i],dp[i-2]+cost[i]) 
+        return min(dp[n-1],dp[n-2])  #reaching last is min of last two
         
