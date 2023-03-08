264478141552f70e477f564e359990992f333582
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-09 02:06:31 +0530

    Time: 160 ms (27.17%), Space: 15.9 MB (18.97%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
@@ -1,33 +1,34 @@
 class Solution:
-    def isvalid(self,a):
+    #https://www.youtube.com/watch?v=FZaUh9vVcKc
+    def isvalid(self,a): #to make a upper and lower bound check
         return a>=0 and a<=6000
     def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
         from collections import deque
         q=deque()
-        forbidden=set(forbidden)
-        visited=set()
-        q.append([0,False])
-        visited.add((0,False))
+        forbidden=set(forbidden) #to make search easier
+        visited=set() #already visited
+        q.append([0,False]) #position,isbackward(have we reached here by backward move)
+        visited.add((0,False)) #Poistion 0 is visited by forward move
         ans=0
         while len(q)>0:
             size=len(q)
             for i in range(size):
                 curr,isback=q.popleft()
-                if curr==x:
+                if curr==x: #got the ans
                     return ans
                 if isback==False:
                     #move forward
                     if ((curr+a),False) not in visited and curr+a not in forbidden and self.isvalid(curr+a):
-                        visited.add(((curr+a),False))
+                        visited.add(((curr+a),False)) #visited by forward move
                         q.append([curr+a,False])
                     #move backward
                     if ((curr-b,True)) not in visited and curr-b not in forbidden and self.isvalid(curr-b):
-                        visited.add(((curr-b),True))
+                        visited.add(((curr-b),True)) #visited by backward move
                         q.append([curr-b,True])
                 else:
                     #just move forward
                     if ((curr+a),False) not in visited and curr+a not in forbidden and self.isvalid(curr+a):
-                        visited.add(((curr+a),False))
+                        visited.add(((curr+a),False)) #visited by forward move
                         q.append([curr+a,False])
                         
             #print(visited)
