8103a4a3ad46d68453202f6910e8a1a984714911
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-10 01:18:53 +0530

    Time: 9475 ms (5.15%), Space: 330 MB (5.15%) - LeetHub

--- 0805-split-array-with-same-average/0805-split-array-with-same-average.py
+++ 0805-split-array-with-same-average/0805-split-array-with-same-average.py
@@ -1,29 +1,20 @@
 class Solution:
-    def solve(self,nums,i,n,sums,count,dp):
-        if sums==0 and count==0:
+    def solve(self, nums, i, n, sums, count, dp):
+        if count == 0 and sums == 0:
             return True
-        if sums<0 or count<0:
+        if i == n or count < 0 or sums < 0:
             return False
-        if i==n:
-            return False
-        if dp[sums][count]!=-1:
-            return dp[sums][count]
-        a=self.solve(nums,i+1,n,sums-nums[i],count-1,dp)
-        b=self.solve(nums,i+1,n,sums,count,dp)
-        dp[sums][count]=a or b
-        return dp[sums][count]
-        
+        if dp[i][count][sums] != -1:
+            return dp[i][count][sums]
+        dp[i][count][sums] = self.solve(nums, i+1, n, sums-nums[i], count-1, dp) or self.solve(nums, i+1, n, sums, count, dp)
+        return dp[i][count][sums]
+
     def splitArraySameAverage(self, nums: List[int]) -> bool:
-        n=len(nums)
-        summ=sum(nums)
-        if nums==[3,4,9,4,4,3,9,8,5,3] or nums==[9664,7110,111,1705,3837,8487,6422,9526,5425,6205,9715,1584,8074,5273,9475,3798,4990,1105]:
-            return True
-        for i in range(1,n):
-            if (summ*i)%n==0:
-                tsum=(summ*i)//n
-                dp=[[-1 for j in range(i+1)] for k in range(tsum+1)]
-                tans=self.solve(nums,0,n,tsum,i,dp)
-                if tans==True:
+        n, summ = len(nums), sum(nums)
+        for i in range(1, n):
+            if summ * i % n == 0:
+                tsum = summ * i // n
+                dp = [[[-1 for _ in range(tsum+1)] for _ in range(i+1)] for _ in range(n+1)]
+                if self.solve(nums, 0, n, tsum, i, dp):
                     return True
-        return False        
-        
+        return False
