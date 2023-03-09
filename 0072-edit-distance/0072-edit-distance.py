e41438a25042ad84c2f0c1f71d708a86b122cf42
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-09 17:34:18 +0530

    Time: 87 ms (91.89%), Space: 17.2 MB (70.37%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,15 +1,16 @@
 class Solution:
     def solve(self,s1,s2,m,n,dp):
+        #it is very important to note that what is to be returned, as per the return type of the function(count,min/max,sum etc)
         if m==0:
-            return n
+            return n #require n moves
         if n==0:
-            return m
+            return m #require m moves
         if dp[m][n]!=-1:
             return dp[m][n]
         if s1[m-1]==s2[n-1]:
-            dp[m][n]=self.solve(s1,s2,m-1,n-1,dp)
+            dp[m][n]=self.solve(s1,s2,m-1,n-1,dp) #if both are same then find in remianing
         else:
-            dp[m][n]=min(1+self.solve(s1,s2,m-1,n,dp),1+self.solve(s1,s2,m,n-1,dp),1+self.solve(s1,s2,m-1,n-1,dp))
+            dp[m][n]=min(1+self.solve(s1,s2,m-1,n,dp),1+self.solve(s1,s2,m,n-1,dp),1+self.solve(s1,s2,m-1,n-1,dp)) #min of deletion,insertion and replacement, adding 1 because 1 move have been made
         return dp[m][n]  
     
     def minDistance(self, word1: str, word2: str) -> int:
