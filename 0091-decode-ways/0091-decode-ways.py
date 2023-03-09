192b4c1cb8ab7d464220da1a7018617a61ad3f8d
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-09 15:27:59 +0530

    Time: 32 ms (75.54%), Space: 13.9 MB (71.44%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,16 +1,18 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        #https://www.youtube.com/watch?v=jFZmBQ569So
+        #make 4 cases , last two are (0,0),(0,non 0),(non 0,0),(non 0,non 0)
         n=len(s)
         dp=[0 for i in range(n)]
         if s[0]=='0':
             dp[0]=0
         else:
             dp[0]=1
-        if n==1 and s[0]=='0':
-            return 0
+        
+        #make cases and see
         for i in range(1,n):
             if s[i]!='0' and s[i-1]!='0':
-                if int(s[i-1:i+1])<=26:
+                if int(s[i-1:i+1])<=26: #if value is less than 26
                     dp[i]=dp[i-1]+(dp[i-2] if i-2>=0 else 1)
                 else:
                     dp[i]=dp[i-1]
