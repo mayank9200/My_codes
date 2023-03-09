e764541824c4318f12b8d7ffc39d0f55f60c0902
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-10 01:31:25 +0530

    Time: 9424 ms (5.15%), Space: 330.1 MB (5.15%) - LeetHub

--- 0805-split-array-with-same-average/0805-split-array-with-same-average.py
+++ 0805-split-array-with-same-average/0805-split-array-with-same-average.py
@@ -1,5 +1,5 @@
 class Solution:
-    def solve(self, nums, i, n, sums, count, dp):
+    def solve(self, nums, i, n, sums, count, dp): #to find if there a sum exist with i numbers
         if count == 0 and sums == 0:
             return True
         if i == n or count < 0 or sums < 0:
@@ -10,11 +10,17 @@ class Solution:
         return dp[i][count][sums]
 
     def splitArraySameAverage(self, nums: List[int]) -> bool:
+        #https://www.youtube.com/watch?v=VwylCVAVdmo
+        # s1//n1==s2//n1
+        # s1+s2=summ
+        # n1+n2=n
+        # find value of s1 ie s1=(summ*n1)//n
+        # now for all possible s1 and corresponding n1 check if this combination is possible
         n, summ = len(nums), sum(nums)
         for i in range(1, n):
-            if summ * i % n == 0:
+            if summ * i % n == 0: #if possible sum is integer then only proceed
                 tsum = summ * i // n
-                dp = [[[-1 for _ in range(tsum+1)] for _ in range(i+1)] for _ in range(n+1)]
+                dp = [[[-1 for _ in range(tsum+1)] for _ in range(i+1)] for _ in range(n+1)] #for memorization since three variables are changing,hence a 3d dp
                 if self.solve(nums, 0, n, tsum, i, dp):
                     return True
         return False
