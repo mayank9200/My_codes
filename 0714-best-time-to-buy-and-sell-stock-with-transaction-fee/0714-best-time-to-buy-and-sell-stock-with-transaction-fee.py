0d2e151badf46bbdead2b25a86536220bd67bc70
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-16 09:19:49 +0530

    Time: 869 ms (53.58%), Space: 21.2 MB (74.50%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,14 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         #https://www.youtube.com/watch?v=pTQB9wbIpfU&list=PL-Jc9J83PIiG8fE6rj9F5a6uyQ5WPdqKy&index=32
+        #maintain two states
+        # one brought state where brought> sell
+        # one sell state where brought=sell
+        #see gradually how current result depends on previous result
+#         [-1, -1, -1, -1, 1, 1]
+#         [0, 0, 0, 5, 5, 8]
+        
+        
         n=len(prices)
         dpbs=[0 for i in range(n)]
         dpss=[0 for i in range(n)]
-        dpbs[0]=-prices[0]
-        dpss[0]=0
+        dpbs[0]=-prices[0] #buy state
+        dpss[0]=0 #sell state
         for i in range(1,n):
-            dpbs[i]=max(dpbs[i-1],dpss[i-1]-prices[i])
-            dpss[i]=max(dpss[i-1],dpbs[i-1]+prices[i]-fee)
+            dpbs[i]=max(dpbs[i-1],dpss[i-1]-prices[i]) #either take previous bought state or buy current stock over previous selling
+            dpss[i]=max(dpss[i-1],dpbs[i-1]+prices[i]-fee) #either take previous selling or sell the previous bought by deducting the fee
         return dpss[n-1]    
 
             
