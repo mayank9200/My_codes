c4ef2023621243ea7704d5889cd60951d609ee7b
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-17 17:39:25 +0530

    Time: 647 ms (82.37%), Space: 16.9 MB (61.67%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,4 +1,5 @@
 class Solution:
+    #https://www.youtube.com/watch?v=UagRoA3C5VQ&list=PL-Jc9J83PIiEZvXCn-c5UIBvfT8dA-8EG
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         m=len(matrix)
         n=len(matrix[0])
@@ -13,7 +14,7 @@ class Solution:
         for i in range(1,m):
             for j in range(1,n):
                 if matrix[i][j]=='1':
-                    dp[i][j]=1+min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
+                    dp[i][j]=1+min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1]) #min wale ke sath milke ek bada square bana lega
                 maxx=max(maxx,dp[i][j])    
           
         return maxx*maxx
