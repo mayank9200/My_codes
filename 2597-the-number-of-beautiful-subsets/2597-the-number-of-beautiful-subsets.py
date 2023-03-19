563c302d4e8c5ca5a7a5ad7cf477b5a726c671fc
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-19 12:15:54 +0530

    Time: 4720 ms (7.69%), Space: 14 MB (53.85%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
@@ -3,21 +3,20 @@ class Solution:
         if ind==n:
             ans[0]+=1
             return  
-        if v[ip[ind]-k]==0:
-            v[ip[ind]]+=1
-            self.solve(ip,op+[ip[ind]],ind+1,n,k,ans,v)
-            v[ip[ind]]-=1
-        self.solve(ip,op,ind+1,n,k,ans,v)
+        if v[ip[ind]-k]==0: #if element-k not in taken till now
+            v[ip[ind]]+=1 #will take that element
+            self.solve(ip,op+[ip[ind]],ind+1,n,k,ans,v) #case for taking
+            v[ip[ind]]-=1 #will remove that element
+        self.solve(ip,op,ind+1,n,k,ans,v) #case for not taking
         return
         
         
     def beautifulSubsets(self, nums: List[int], k: int) -> int:
         n=len(nums)
+        nums.sort() #sort just to see nums[i]-k in array
         ans=[0]
-        nums.sort()
         op=[]
-        s=set()
-        v=[0 for i in range(1001)]
+        v=[0 for i in range(1001)] 
         self.solve(nums,[],0,n,k,ans,v)
         return ans[0]-1
         
