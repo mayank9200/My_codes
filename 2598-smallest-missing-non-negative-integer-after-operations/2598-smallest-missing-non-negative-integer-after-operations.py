bb2ead5a1525ea2b415d84a8ff8891311e2806f5
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-19 14:05:30 +0530

    Time: 1142 ms (100.00%), Space: 44.7 MB (100.00%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.py
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.py
@@ -1,12 +1,11 @@
 class Solution:
     def findSmallestInteger(self, nums: List[int], value: int) -> int:
-        m = Counter([n % value for n in nums])
+        m = Counter([n % value for n in nums]) #creating frequency of min value that it can make
         print(m)
         for i in range(len(nums)):
-            print(i,value,i%value)
-            if m[i % value] == 0:
+            if m[i % value] == 0: #starting from 0 see if curr value can be made from element and it has a frequency greater than 0
                 return i
-            m[i % value] -= 1 
+            m[i % value] -= 1 #made one element so subtract its frequency
         return len(nums)  
                  
             
