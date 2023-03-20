8bc294daa04da096669d51430639310172045902
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-21 01:04:27 +0530

    Time: 3256 ms (56.12%), Space: 14.2 MB (16.33%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def minimumMountainRemovals(self, nums: List[int]) -> int:
+        #https://www.youtube.com/watch?v=jdfpGSSyN2I&list=PL-Jc9J83PIiEZvXCn-c5UIBvfT8dA-8EG&index=11
         n=len(nums)
         lis=[0 for i in range(n)]
         lds=[0 for i in range(n)]
@@ -12,6 +13,7 @@ class Solution:
                 if nums[j]<nums[i]:
                     maxx=max(maxx,lis[j])
             lis[i]=maxx+1
+        #find lds(longest decreasing subsequence)    
         for i in range(n-2,-1,-1):
             maxx=0
             for j in range(i+1,n):
@@ -20,9 +22,9 @@ class Solution:
             lds[i]=maxx+1
         max_bitnoic=0
         for i in range(n):
-            if lis[i]>1 and lds[i]>1:
-                max_bitnoic=max(max_bitnoic,lis[i]+lds[i]-1)
-        return n-max_bitnoic    
+            if lis[i]>1 and lds[i]>1: #if any one of them has value 1 means it is strictly increasing/decreasing but we have to find mountain hence both should be greater than 1
+                max_bitnoic=max(max_bitnoic,lis[i]+lds[i]-1) #find max bitnoic array length
+        return n-max_bitnoic #minimum removals are total elements-elements involved in max bitnoic array
             
         
         
