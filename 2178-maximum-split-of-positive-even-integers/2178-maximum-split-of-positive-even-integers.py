73b9d4a1baff5db869161f85656610ecc007a887
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-22 00:55:29 +0530

    Time: 605 ms (18.32%), Space: 26.6 MB (71.79%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.py
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.py
@@ -1,17 +1,16 @@
 class Solution:
     def maximumEvenSplit(self, finalSum: int) -> List[int]:
-        s=deque()
         if finalSum%2!=0:
             return []
         ans=[]
         currsumm=0
         i=0
-        while currsumm+(2*(i+1))<=finalSum:
+        while currsumm+(2*(i+1))<=finalSum: #go till that point where the we found k-1 integers
             i+=1
             currsumm=currsumm+(2*i)
-            ans.append(2*i)
+            ans.append(2*i) #making the ans
         #print(finalSum,currsumm) 
-        ans[-1]=ans[-1]+(finalSum-currsumm)    
+        ans[-1]=ans[-1]+(finalSum-currsumm)  #the last element will just be incremented by the remaining value  
         return ans
             
     
