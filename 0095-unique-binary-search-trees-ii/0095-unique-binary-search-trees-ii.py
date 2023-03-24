1b3d682dbdc742022a409968af9744664e967ba5
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-24 10:35:21 +0530

    Time: 57 ms (71.01%), Space: 15.7 MB (35.84%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
@@ -6,22 +6,25 @@
 #         self.right = right
 class Solution:
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        #https://www.youtube.com/watch?v=qOItdXuTZGo
+        #somewhat similar to create tree from preorder and inorder
         ans=[]
-        start=1
-        end=n
+        start=1 #start from 1
+        end=n #end at n
         def helper(n,start,end):
-            if start>end:
-                return [None]
+            if start>end:  #if they cross each other
+                return [None] #only None will be in list
             ans=[]
-            for k in range(start,end+1):
-                left=helper(n,start,k-1)
-                right=helper(n,k+1,end)
+            for k in range(start,end+1): #make everyone root
+                left=helper(n,start,k-1) #gives left root list
+                right=helper(n,k+1,end) #gives right root list
+                #make every combination of left with right(left=m,right=n, so trees with this root will be left*right)
                 for i in left:
                     for j in right:
-                        root=TreeNode(k)
-                        root.left=i
-                        root.right=j
-                        ans.append(root)
-            return ans  
+                        root=TreeNode(k) #make k root
+                        root.left=i #do left connection
+                        root.right=j #do right connection
+                        ans.append(root) #append one ans
+            return ans  #return that ans
         return helper(n,start,end)
         
