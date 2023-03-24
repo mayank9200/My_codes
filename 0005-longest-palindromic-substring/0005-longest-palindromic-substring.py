14b7aabe63a771e396b0b119757f9db2ff617079
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-24 16:33:52 +0530

    Time: 1610 ms (36.54%), Space: 14.1 MB (24.94%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,42 +1,37 @@
 class Solution:
     def isvalid(self,s,i):
         return i>=0 and i<len(s)
+    def longest_palindrome(self,s,prev,nextt,tans,fans):
+        while self.isvalid(s,prev) and self.isvalid(s,nextt):
+            if s[prev]==s[nextt]:
+                tans=s[prev]+tans
+                tans=tans+s[nextt]
+                prev-=1
+                nextt+=1
+            else:
+                break
+        if len(tans)>len(fans):
+            fans=tans
+        return fans    
+
         
     def longestPalindrome(self, s: str) -> str:
         n=len(s)
         maxx=0
         ans=''
+        fans=''
         for i in range(n):
-            prev=i-1
-            nextt=i+1
+            #odd
             tans=s[i]
-            while self.isvalid(s,prev) and self.isvalid(s,nextt):
-                if s[prev]==s[nextt]:
-                    tans=s[prev]+tans
-                    tans=tans+s[nextt]
-                    prev-=1
-                    nextt+=1
-                else:
-                    break
-               
-            if len(tans)>maxx:
-                #print(tans)
-                maxx=len(tans)
-                fans=tans
-            if i+1<n and s[i]==s[i+1]: 
-                prev=i-1
-                nextt=i+2
+            func=self.longest_palindrome(s,i-1,i+1,tans,fans)
+            if len(func)>len(ans):
+                ans=func
+            #even
+            if i+1<n and s[i]==s[i+1]:
                 tans=s[i]+s[i+1]
-                while self.isvalid(s,prev) and self.isvalid(s,nextt):
-                    if s[prev]==s[nextt]:
-                        tans=s[prev]+tans
-                        tans=tans+s[nextt]
-                        prev-=1
-                        nextt+=1
-                    else:
-                        break
-                if len(tans)>maxx:
-                    maxx=len(tans)
-                    fans=tans
-        return fans
+                func=self.longest_palindrome(s,i-1,i+2,tans,fans)
+                if len(func)>len(ans):
+                    ans=func
+        return ans        
+                
             
