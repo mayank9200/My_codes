26d9e0c1fca48a4551412b5bcf7f1ee9b6fd2e4a
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-26 13:29:32 +0530

    Time: 1243 ms (21.43%), Space: 13.9 MB (100.00%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.py
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.py
@@ -1,17 +1,5 @@
-class Solution:
-    # def findprime(self,n):
-        # ans=[]
-        # is_prime = [True] * (n+1)
-        # is_prime[0] = is_prime[1] = False
-        # for p in range(2, int(n**0.5)+1):
-        #     if is_prime[p]:
-        #         for i in range(p*p, n+1, p):
-        #             is_prime[i] = False
-        # for i in range(2, n+1):
-        #     if is_prime[i]:
-        #         ans.append(i)
-        # return ans    
-    def findprime(self,n):
+class Solution:  
+    def findprime(self,n): #print primes upto n
         ans=[]
         for i in range(2,n+1):
             count1=0
@@ -23,7 +11,7 @@ class Solution:
             if count1==count2:
                 ans.append(i)    
         return ans    
-    def justgreater(self,primes,val):
+    def justgreater(self,primes,val): #find just greater element in prime using binary search
         start=0
         end=len(primes)-1
         candi=primes[0]
@@ -36,21 +24,17 @@ class Solution:
                 end=mid-1
         return candi       
     def primeSubOperation(self, nums: List[int]) -> bool:
-        primes=self.findprime(1001)
-        #print(primes)
+        primes=self.findprime(1001) #print primes upto 1000
         for i in range(len(nums)-2,-1,-1):
             counter=0
             val=nums[i]
-            if nums[i]<nums[i+1]:
-                continue
-            #print(nums[i])    
-            f=self.justgreater(primes,nums[i]-nums[i+1])  
-            #print(nums[i]-nums[i+1],f)
-            nums[i]=nums[i]-f
-        #print(nums)
+            if nums[i]<nums[i+1]:  #if already less than dont subtract
+                continue  
+            f=self.justgreater(primes,nums[i]-nums[i+1])  #see the diff and find value just greater
+            nums[i]=nums[i]-f #subtract
         if nums[0]<=0:
             return False
-        for i in range(1,len(nums)):
+        for i in range(1,len(nums)): #see if the array is strictly decreasing
             if nums[i]<=0:
                 return False
             if nums[i]<nums[i-1]:
