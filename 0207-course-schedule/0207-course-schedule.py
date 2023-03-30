58c6d26df20f10e2493cc43be6aa28f6fd8582be
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-30 21:29:16 +0530

    Time: 97 ms (80.01%), Space: 15.4 MB (85.14%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,28 +1,27 @@
 class Solution:
+    #topological sort to check if there is no cycle in directed graph
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         from collections import deque
         q=deque()
         count=0
-        indegree=[0 for i in range(numCourses)]
-        adj=[[] for i in range(numCourses)]
+        indegree=[0 for i in range(numCourses)] #storing indegrees
+        adj=[[] for i in range(numCourses)] #adj list
         for i in prerequisites:
             indegree[i[0]]+=1
             adj[i[1]].append(i[0])
         for i in range(numCourses):
-            if indegree[i]==0:
+            if indegree[i]==0: #if indegree is 0 put in queue
                 q.append(i)
                 count+=1
         
         while len(q)>0:
             node=q.popleft()
             for i in adj[node]:
-                indegree[i]-=1
-                if indegree[i]==0:
+                indegree[i]-=1 #reduce indegree
+                if indegree[i]==0: #if indegree becomes 0 append it to queue
                     q.append(i)
                     count+=1
-        return count==numCourses            
+        return count==numCourses   #see if all nodes are visited         
                     
-        print(indegree)
-        print(adj)
-        return True
+      
         
