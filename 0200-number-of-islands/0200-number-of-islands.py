7482f49025e626c1df3ef9efe1079572bbd69039
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-03-30 11:16:55 +0530

    Time: 316 ms (51.37%), Space: 16.4 MB (62.61%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,8 +1,9 @@
 class Solution:
     def dfs(self,i,j,grid,visited):
         if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or visited[i][j]==True or grid[i][j]=='0':
-            return
-        visited[i][j]=True
+            return #coming out or grid or invalid position or already visited position
+        visited[i][j]=True #make it visited
+        #go to all 4 surrounging
         self.dfs(i+1,j,grid,visited)
         self.dfs(i-1,j,grid,visited)
         self.dfs(i,j+1,grid,visited)
@@ -16,7 +17,7 @@ class Solution:
         count=0
         for i in range(m):
             for j in range(n):
-                if visited[i][j]==False and grid[i][j]=='1':
+                if visited[i][j]==False and grid[i][j]=='1': #if valid
                     self.dfs(i,j,grid,visited)
                     count+=1
         return count        
