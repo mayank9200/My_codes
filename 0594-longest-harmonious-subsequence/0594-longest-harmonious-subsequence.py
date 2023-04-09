72c25ec7bdd9e67686e63fab65ceb0435a5f3d35
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-04-09 22:48:46 +0530

    Time: 327 ms (37.82%), Space: 15.8 MB (89.43%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -3,10 +3,10 @@ class Solution:
     def findLHS(self, nums: List[int]) -> int:
         #https://leetcode.com/problems/longest-harmonious-subsequence/discuss/103534/Python-Straightforward-with-Explanation
         count={}
-        for i in nums:
-            count[i]=count.get(i,0)+1
+        for i in nums: 
+            count[i]=count.get(i,0)+1 #storing the frequency of each number
         maxx=0    
         for i in nums:
             if i+1 in count:
-                maxx=max(maxx,count[i]+count[i+1])
+                maxx=max(maxx,count[i]+count[i+1]) #maximum value of all the occurances if i and i+1
         return maxx        
