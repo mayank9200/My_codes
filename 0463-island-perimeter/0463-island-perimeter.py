8d397a36aed7729d0f088e43a3e8ce43971760ca
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-04-18 18:50:21 +0530

    Time: 686 ms (18.01%), Space: 18.7 MB (28.33%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,13 +1,28 @@
 class Solution:
+    def isvalid(self,i,j,m,n):
+        return i>=0 and j>=0 and i<m and j<n
+    def dfs(self,i,j,grid,peri):
+        m=len(grid)
+        n=len(grid[0])
+        if self.isvalid(i,j,m,n)==False or grid[i][j]==0:
+            peri[0]+=1
+            return
+        if grid[i][j]==-1:
+            return
+        grid[i][j]=-1
+        self.dfs(i+1,j,grid,peri)
+        self.dfs(i,j+1,grid,peri)
+        self.dfs(i-1,j,grid,peri)
+        self.dfs(i,j-1,grid,peri)
+        return
+    
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        m, n, Perimeter = len(grid), len(grid[0]), 0
-        for i in range(m):
-            for j in range(n):
-                Perimeter += 4*grid[i][j]
-                if i > 0:   Perimeter -= grid[i][j]*grid[i-1][j]
-                if i < m-1: Perimeter -= grid[i][j]*grid[i+1][j]
-                if j > 0:   Perimeter -= grid[i][j]*grid[i][j-1]
-                if j < n-1: Perimeter -= grid[i][j]*grid[i][j+1]
-                    
-        return Perimeter
+        peri=[0]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==1:
+                    self.dfs(i,j,grid,peri)
+       
+        return peri[0]
+        #do again
         
