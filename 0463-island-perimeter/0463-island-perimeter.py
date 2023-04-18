3b63dc35ba86fa1d78c8619486b8ea9fd1f86066
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-04-18 18:51:42 +0530

    Time: 676 ms (19.58%), Space: 18.7 MB (28.33%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,15 +1,17 @@
 class Solution:
     def isvalid(self,i,j,m,n):
-        return i>=0 and j>=0 and i<m and j<n
+        return i>=0 and j>=0 and i<m and j<n #to check if it goes out of the island
     def dfs(self,i,j,grid,peri):
         m=len(grid)
         n=len(grid[0])
-        if self.isvalid(i,j,m,n)==False or grid[i][j]==0:
+        if self.isvalid(i,j,m,n)==False or grid[i][j]==0: #if goes out or surrounding is water then increase peri
             peri[0]+=1
             return
-        if grid[i][j]==-1:
+        if grid[i][j]==-1: #if reached a visited cell
             return
-        grid[i][j]=-1
+        grid[i][j]=-1 #make visited
+        #go to all 4 directions
+
         self.dfs(i+1,j,grid,peri)
         self.dfs(i,j+1,grid,peri)
         self.dfs(i-1,j,grid,peri)
