638b2d788ad1f26cd3891ece58289b5d65061058
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-04-19 00:56:47 +0530

    Time: 34 ms (49.77%), Space: 14 MB (8.98%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -3,11 +3,11 @@ class Solution:
         #dfs to calculate product till we reach the destination edge
         def dfs(src,dest,d,visited,prod):
             if src==dest: #reached destination
-                return prod[0]
-            visited.add(src)
+                return prod[0] #got the required product
+            visited.add(src) #making it visited
             for i in d[src]:
                 if i[0] not in visited:
-                    prod[0]=prod[0]*i[1]
+                    prod[0]=prod[0]*i[1] #multiplying till that point
                     tans=dfs(i[0],dest,d,visited,prod) #see if we are getting any ans
                     prod[0]=prod[0]/i[1] #backtrack
                     if tans!=-1: #if answer exist
@@ -24,7 +24,7 @@ class Solution:
                 d[equations[i][0]].append([equations[i][1],values[i]])
                 
             if equations[i][1] not in d:
-                d[equations[i][1]]=[[equations[i][0],1/values[i]]]
+                d[equations[i][1]]=[[equations[i][0],1/values[i]]] #reverse edges with reverse weights
             else:
                 d[equations[i][1]].append([equations[i][0],1/values[i]])
         ans=[]
@@ -32,9 +32,9 @@ class Solution:
         for q in queries:
             prod=[1]
             visited=set()
-            if q[0] not in d or q[1] not in d:
+            if q[0] not in d or q[1] not in d: #if val is not in d
                 ans.append(-1.0)
             else:    
-                val=dfs(q[0],q[1],d,visited,prod)
+                val=dfs(q[0],q[1],d,visited,prod) #make dfs to get required product
                 ans.append(val)     
-        return ans
+        return ans #final answer
