c6b1b500766d8ebb1893b0ec52d05dacfdd98f11
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-04-19 15:31:51 +0530

    Time: 284 ms (74.23%), Space: 15.8 MB (79.38%) - LeetHub

--- 1311-get-watched-videos-by-your-friends/1311-get-watched-videos-by-your-friends.py
+++ 1311-get-watched-videos-by-your-friends/1311-get-watched-videos-by-your-friends.py
@@ -1,34 +1,36 @@
 class Solution:
     def watchedVideosByFriends(self, watchedVideos: List[List[str]], friends: List[List[int]], id: int, level: int) -> List[str]:
+        #simple level order traversal, where for the level we fing the frequencies of watchedVideos and sort them in ascending order
+        #friends is the adjacency list
         from collections import deque
         q=deque()
-        q.append([id,0])
+        q.append([id,0]) #id and its level
         visited=[False for i in range(len(watchedVideos))]
         visited[id]=True
         ans=[]
         while len(q)>0:
             size=len(q)
             for i in range(size):
-                node,lev=q.popleft()
-                if lev>level:
+                node,lev=q.popleft() #
+                if lev>level: #exceeded level
                     break
-                if lev==level:
+                if lev==level: #reached level
                     ans.append(node)
-                for j in friends[node]:
+                for j in friends[node]: #going to all its neighbors
                     if visited[j]==False:
-                        q.append([j,lev+1])
+                        q.append([j,lev+1]) 
                         visited[j]=True
         d={}
         fans=[]
         for i in ans:
             for j in watchedVideos[i]:
-                d[j]=d.get(j,0)+1
+                d[j]=d.get(j,0)+1 #making frequency hashmap
         tans=[]
         for i in d:
-            tans.append([d[i],i])
-        tans.sort()
+            tans.append([d[i],i]) #appending freq,alphabet
+        tans.sort() #sorting them
         for i in tans:
-            fans.append(i[1])
+            fans.append(i[1]) #appending the alphabets
         return fans    
                          
             
