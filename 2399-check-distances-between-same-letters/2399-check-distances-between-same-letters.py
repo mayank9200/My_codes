26c6a8c2345b441e559bf2a914c9e5f6ad27ce2d
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-04-30 01:08:45 +0530

    Time: 57 ms (7.63%), Space: 16.2 MB (5.31%) - LeetHub

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.py
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.py
@@ -1,16 +1,15 @@
 class Solution:
     def checkDistances(self, s: str, distance: List[int]) -> bool:
-        d={}
+        d={} #dictionary with key as char and position as two values
         n=len(s)
         for i in range(n):
             if s[i] not in d:
                 d[s[i]]=[i]
             else:
-                d[s[i]].append(i)
-        #print(d)        
+                d[s[i]].append(i)      
         for i in range(len(distance)):
             val=chr(ord('a')+i)
-            if val not in d:
+            if val not in d: 
                 continue
             if d[val][1]-d[val][0]-1!=distance[i]:
                 return False
