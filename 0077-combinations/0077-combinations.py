2f98fca6cd682683c82bab90bba366f5b6961b59
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-05-08 01:20:04 +0530

    Time: 502 ms (16.25%), Space: 18.3 MB (9.78%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,13 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        #do again
+        ret = []
+        self.dfs(list(range(1, n+1)), k, [], ret)
+        return ret
+    
+    def dfs(self, nums, k, path, ret):
+        if len(path) == k:
+            ret.append(path)
+            return 
+        for i in range(len(nums)):
+            self.dfs(nums[i+1:], k, path+[nums[i]], ret)
