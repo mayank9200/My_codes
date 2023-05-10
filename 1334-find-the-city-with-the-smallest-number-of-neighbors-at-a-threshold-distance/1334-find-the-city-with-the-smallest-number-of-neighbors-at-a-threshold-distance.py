cd6283e5235557c2d05724869a733f3afe3f4d59
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-05-10 16:31:12 +0530

    Time: 814 ms (48.03%), Space: 17.4 MB (15.81%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,4 +1,6 @@
 class Solution:
+    #floydd warshal algorithm
+    #mat=shortest distance from i to j considering all paths
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
         mat=[[float('inf') for i in range(n)]for j in range(n)]
         for edge in edges:
@@ -9,15 +11,17 @@ class Solution:
         for k in range(n):
             for i in range(n):
                 for j in range(n):
-                    mat[i][j]=min(mat[i][j],mat[i][k]+mat[k][j])
+                    mat[i][j]=min(mat[i][j],mat[i][k]+mat[k][j]) #direct or via k, which is smaller
         res=0
         totcount=float('inf')
         for i in range(n):
             tcount=0
             for j in range(n):
-                if mat[i][j]!=0 and mat[i][j]<=distanceThreshold:
+                if mat[i][j]!=0 and mat[i][j]<=distanceThreshold: #in each col, how many are less than threshold
                     tcount+=1
             if tcount<=totcount: 
                 res=i
                 totcount=tcount
         return res        
+    
+    #TC: O(n^3)
