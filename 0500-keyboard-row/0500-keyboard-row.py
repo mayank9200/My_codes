4d99dbcd16ff931fd3f97167d3c8c578ecd8dc86
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-05-13 00:25:39 +0530

    Time: 35 ms (47.24%), Space: 16.2 MB (14.92%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        d={}
+        ans=[]
+        s=['qwertyuiop','asdfghjkl','zxcvbnm']
+        for i in range(len(s)):
+            for j in s[i]:
+                d[j]=i
+                d[j.upper()]=i     
+        for i in words:
+            found=False
+            temp=d[i[0]]
+            for j in i:
+                if d[j]==temp:
+                    continue
+                found=True
+                break
+                
+            if found==False:
+                ans.append(i)
+        return ans        
+                    
+        
