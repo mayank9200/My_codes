853850cadc21c1fc5ee43d972f4e6ea1984b814b
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-05-24 01:20:25 +0530

    Time: 35 ms (67.91%), Space: 16.4 MB (9.62%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        i=0
+        n=len(rings)
+        ans=0
+        d={}
+        while i<n:
+            a=rings[i]
+            b=rings[i+1]  
+            if b in d:
+                d[b].add(a)
+            else:
+                d[b]=set(a)
+            i+=2    
+        #print(d)    
+        for i in d:
+            if len(d[i])==3:
+                ans+=1
+        return ans        
+        
