b9d431e3475e7635ff8fada7088fa729dd75b6b2
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-05-28 19:34:32 +0530

    Time: 691 ms (80.68%), Space: 17 MB (38.68%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -0,0 +1,28 @@
+class Solution:
+    def dfs(self,src,adj,visited,counte,countn):
+        countn[0]+=1
+        visited[src]=True
+        for i in adj[src]:
+            counte[0]+=1
+            if visited[i]==False:
+                self.dfs(i,adj,visited,counte,countn)
+                
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        visited=[False for i in range(n)] 
+        count=0
+        adj=[[] for i in range(n)]
+        for i in edges:
+            adj[i[0]].append(i[1])
+            adj[i[1]].append(i[0])
+        for i in range(n):
+            if visited[i]==False:
+                counte=[0]
+                countn=[0]
+                self.dfs(i,adj,visited,counte,countn)
+                print(countn,counte)
+                if (countn[0])*(countn[0]-1)==counte[0]:
+                    count+=1
+        #print(visited)               
+        return count   
+        
+        
