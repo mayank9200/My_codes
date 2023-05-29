ca3ce80d055132dc151ba678c58e169a64172d80
Mayank Sharma <64470374+mayank9200@users.noreply.github.com>
2023-05-29 18:22:59 +0530

    Time: 41 ms (41.55%), Space: 16.2 MB (39.04%) - LeetHub

---
+++ 1078-occurrences-after-bigram/1078-occurrences-after-bigram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        
+        results = []
+        text = text.split(" ")
+        for i in range(len(text)-2):
+            if text[i] == first and text[i+1] == second:
+                results.append(text[i+2])
+        return results
